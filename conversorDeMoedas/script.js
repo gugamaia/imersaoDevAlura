5f61098c2cbe5778fb8655ef368fbd8b0a3c27cd
Gustavo Maia <gustavojv_maia@outlook.com>
2022-03-10 11:25:13 -0300

    Initial Coding

---
+++ conversorDeMoedas/script.js
@@ -0,0 +1,11 @@
+let nome = "Gustavo Maia";
+let notaDoPrimeiroBimestre = 9.324;
+let notaDoSegundoBimestre = 7.25;
+let notaDoTerceiroBimestre = 3.1416;
+let notaDoQuartoBimestre = 8;
+let notaFinal = (notaDoPrimeiroBimestre + notaDoSegundoBimestre + notaDoTerceiroBimestre + notaDoQuartoBimestre) / 4
+
+let notaFixada = notaFinal.toFixed(1)
+console.log("Bem vindo " + nome);
+console.log(notaFinal);
+console.log(notaFixada);
